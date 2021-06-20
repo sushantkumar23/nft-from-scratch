316a03d59fd52b9590db0aeff68ef05f7f75ad5d
Sushant Kumar <sushantkumar23@gmail.com>
2021-06-20 23:53:54 +0530

    Added AdvancedCollectible contract

--- contracts/AdvancedCollectible.sol
+++ contracts/AdvancedCollectible.sol
@@ -5,12 +5,43 @@ import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
 contract AdvancedCollectible is ERC721, VRFConsumerBase {
     
+    bytes32 internal keyhash;
+    uint256 public fee;
+    uint256 public tokenCounter;
+
+    enum Breed{PUG, SHIBA_INU, ST_BERNARD}
+
+    mapping(bytes32 => address) public requestIdToSender;
+    mapping(bytes32 => string) public requestIdToTokenURI;
+    mapping(uint256 => Breed) public tokenIdToBreed;
+    mapping(bytes32 => uint256) public requestIdToTokenId;
+    event requestedCollectible(bytes32 indexed requestId);
+
     constructor(address _VRFCoordinator, address _LinkToken, bytes32 _keyhash) public
     VRFConsumerBase(_VRFCoordinator, _LinkToken)
     ERC721("Doggies", "DOG")
     {
+        keyhash = _keyhash;
+        fee = 0.1 * 10 ** 18; // 0.1 LINK
+        tokenCounter = 0;
     }
 
     function createCollectible(uint256 userProvidedSeed, string memory tokenURI) public returns(bytes32) {
+        bytes32 requestId = requestRandomness(keyhash, fee, userProvidedSeed);
+        requestIdToSender[requestId] = msg.sender;
+        requestIdToTokenURI[requestIdToSender] = tokenURI;
+        emit requestedCollectible(requestId);
+    }
+
+    function fulfillRandomness(bytes requestId, uint256 randomNumber) internal override{
+        address dogOwner = requestIdToSender[requestId];
+        string memory tokenURI = requestIdToTokenURI[requestId];
+        uint156 newItemId = tokenCounter;
+        _safeMint(dogOwner, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        Breed breed = Breed(randomNumber % 3);
+        tokenIdToBreed[newItemId] = breed;
+        requestIdToTokenId[requestId] = newItemId;
+        tokenCounter = tokenCounter + 1;
     }
 }
